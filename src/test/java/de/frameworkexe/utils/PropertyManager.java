130ad683ade6b6f40c31bc34fb3da9e948b491c9
UwJo <uwjoy1@gmail.com>
2024-02-09 11:21:29 +0200

    catch properties

---
+++ src/test/java/de/frameworkexe/utils/PropertyManager.java
@@ -0,0 +1,41 @@
+package de.frameworkexe.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyManager {
+    //initialize a variable of type Properties holding properties from config.properties
+    private static Properties props = new Properties();
+    
+    //Create a method to fetch the properties from the config.properties file
+
+    public Properties getProperties() throws IOException{
+        InputStream  inputStream = null;
+        String propFileName = "config.properties";
+
+        if(props.isEmpty()){
+            try {
+                //Fetch properties form  config.properties and assing them to inputStream variable
+                inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+                //Assign the fetched properties to props variable
+                props.load(inputStream);
+            }
+            //catch the error if fetching doesn't work
+            catch (IOException e) {
+                //Print the error and its origin 
+                e.printStackTrace();
+                //Print the error and its origin 
+                throw e;
+            }
+            finally{
+                //Close inputStream has been successfully retreived 
+                if (inputStream != null)
+                    inputStream.close();
+            }
+        }
+        return props;
+    }
+    
+}
