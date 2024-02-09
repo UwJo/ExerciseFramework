d30d98307a63ef832c03419976b81698e4c4ae68
UwJo <uwjoy1@gmail.com>
2024-02-09 12:39:50 +0200

    printing drivermanager

---
+++ src/test/java/de/frameworkexe/utils/DriverManager.java
@@ -0,0 +1,41 @@
+package de.frameworkexe.utils;
+
+import java.io.IOException;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class DriverManager {
+    //Create a driver variable of type ThreadLocal<AppiumDriver>
+    private static ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
+
+    //Create a getter method for the driver
+    public AppiumDriver getDriver() {
+        return driver.get();
+
+    }
+    //Create a setter method for the driver
+    public void setDriver(AppiumDriver driverValue){
+        driver.set(driverValue);
+    }
+    //Create a method to initialize the driver 
+    public void initializeDriver(){
+        AppiumDriver driver = null;
+
+        if(driver == null){
+            try{
+            //Create a driver instance using server URL and the Capabilities
+            driver = new AndroidDriver(new ServerManager().getUrl(), new CapabilitiesManager().getCapabilities());
+            //Set the ThreadLocal driver variable to equal to the newly created driver 
+            DriverManager.driver.set(driver);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            throw e;
+        }
+        
+        }
+
+    } 
+   }
+
